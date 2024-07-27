1bf3f1e1078df6263ff4efde2d3773cba615550e
Valid Rabiee <validrabieejs@gmail.com>
2024-07-27 15:23:34 +0330

    Fix HTTPS error

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast {
     constructor(){
         this.key = "aovCMu1gq8Y2yVnWX3MQxG9RTeHl1fbq";
-        this.weatherURI = "http://dataservice.accuweather.com/currentconditions/v1/";
-        this.cityURI = "http://dataservice.accuweather.com/locations/v1/cities/search";
+        this.weatherURI = "https://dataservice.accuweather.com/currentconditions/v1/";
+        this.cityURI = "https://dataservice.accuweather.com/locations/v1/cities/search";
     }
     async updateCity(city){
         const cityDets = await this.getCity(city);
